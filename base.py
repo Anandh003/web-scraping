99bc7057375a9e58c84d3ac6d35d4b6b45f74381
Anandh003 <angaravi003@gmail.com>
2020-10-10 16:42:53 +0530

    Adds base python file
    Signed-off-by: Anandh003 <angaravi003@gmail.com>

---
+++ base.py
@@ -0,0 +1,21 @@
+"""
+    Base Script to have reusable functions
+"""
+
+from urllib.error import HTTPError
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+def get_page(url):
+    try:
+        response = urlopen(url)
+    except HTTPError as e:
+        print('Unable to fetch {}'.format(url))
+        print('Error Message \n {}'.format(e))
+        return None
+    
+    return BeautifulSoup(response, features="html.parser")
+
+
+    
